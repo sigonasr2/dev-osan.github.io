1ec7a366d19947e79e2a44c00fadafda5e39a71f
sigonasr2 <sigonasr2@gmail.com>
2019-05-22 12:02:33 +0900

    Added random HTML picker script to influence the design of devOsan logo and mini logo at random.

---
+++ additionalScripts.js
@@ -0,0 +1,22 @@
+var vals = [
+	`<h1 class="devOsan"">
+              <span style="color: #696035;">{</span>dev<span style="color: #696035;">}</span>Osan
+            </h1>
+            <h4 class="devOsan">Software Development Club</h4>`, //Original
+	`<h1 class="devOsan" style="text-shadow: 2px 2px 6px #AAFFAA;">
+               <span style="color: #696035;">{</span>dev<span style="color: #696035;">}</span>Osan
+            </h1>	
+            <h4 class="devOsan">Software Development Club</h4>`,	//By Sig
+];
+
+var vals2 = [
+	`<span style="color: #696035">{</span>dev<span style="color: #696035">}</span>Osan`, //Original
+	`<span style="color:black;text-shadow: 0px 0px 2px #AAFFAA;"><span style="color: #696035;">{</span>dev<span style="color: #696035">}</span>Osan</span>`, //By Sig	
+];
+
+window.onload=generateLogo(Math.floor(Math.random()*vals.length));
+
+function generateLogo(numb) {
+	document.getElementById("devOsanLogo-dynamic").innerHTML=vals[numb];
+	document.getElementById("devOsanMiniLogo-dynamic").innerHTML=vals2[numb];
+} 
