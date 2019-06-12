0fa35401b59a34067edb2e8d6d8432c3b1067aaf
BuildTools <unconfigured@null.spigotmc.org>
2019-06-12 10:26:57 -0500

    Improved site form and added authentication and querying.

--- sig-ask-devosan/index.php
+++ sig-ask-devosan/index.php
@@ -1,4 +1,23 @@
 <?php
-	
-	echo "Posted Question: <b>".$_POST['question']."</b>";
+	if (strlen($_POST['question'])>0) {
+		$question = $_POST['question'];
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		//echo "Posted Question: <b>".$_POST['question']."</b><br>";
+		if (strlen($_POST['name'])>0) {
+			//echo "Posted Name: <b>".$_POST['name']."</b><br>";
+		}
+		if (strlen($_POST['email'])>0) {
+			//echo "Posted Email: <b>".$_POST['email']."</b><br>";
+		}
+		
+		//Submit the question to a polling list.
+		$file = fopen("./requests/requests.php","a");
+		fwrite($file,$question."
+".$name."
+".$email."
+");
+	} else {
+		echo "-1";
+	}
 ?>
